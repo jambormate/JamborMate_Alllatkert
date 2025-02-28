828b349aba26b6c76ec18be4163051f4eaac5a7b
Petrik Diak <diak@petrik.hu>
2025-02-28 11:09:48 +0100

    MediterranAllat kesz

---
+++ Allatkert/MediterranAllat.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Allatkert
+{
+	internal class MediterranAllat
+	{
+		int sebesseg;
+		string faj;
+		string nev;
+		int suly;
+		string nem;
+
+		public MediterranAllat(int sebesseg, string faj, string nev, int suly, string nem)
+		{
+			this.sebesseg = sebesseg;
+			this.faj = faj;
+			this.nev = nev;
+			this.suly = suly;
+			this.nem = nem;
+		}
+
+		public int Sebesseg { get => sebesseg; set => sebesseg = value; }
+		public string Faj { get => faj; set => faj = value; }
+		public string Nev { get => nev; set => nev = value; }
+		public int Suly { get => suly; set => suly = value; }
+		public string Nem { get => nem; set => nem = value; }
+	}
+}
