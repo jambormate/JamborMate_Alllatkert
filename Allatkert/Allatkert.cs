6dba89c3eda751ac0973d575c34cf8f365df4423
jambormate <jmate2007@gmail.com>
2025-03-05 21:54:34 +0100

    Állatok hozzáadva, kapacítás kiírva

---
+++ Allatkert/Allatkert.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Allatkert
+{
+    internal class Allatkert
+    {
+        List<ViziAllat> viziAllats = new List<ViziAllat>();
+        List<HidegAllat> hidegAllats = new List<HidegAllat>();
+        List<MediterranAllat> mediterranAllats = new List<MediterranAllat>();
+        List<MelegAllat> melegAllats = new List<MelegAllat>();
+        List<TropusiAlllat> tropusiAlllats = new List<TropusiAlllat>();
+        Random rnd = new Random();
+        private int capacity = 0;
+        private int telitetseg = 0;
+
+        public Allatkert()
+        {
+           capacity = rnd.Next(5, 16);
+
+        }
+
+        public void Beolvasas()
+        {
+            viziAllats.Add(new ViziAllat(5, "Fóka", "Ottó", 54, "Hím"));
+            hidegAllats.Add(new HidegAllat(6, "Jegesmedve", "Anna", 231, "Nőstény"));
+            mediterranAllats.Add(new MediterranAllat(4, "Nyúl", "Oreo", 3, "Hím"));
+            melegAllats.Add(new MelegAllat(3, "Teve", "Döme", 457, "Hím"));
+            tropusiAlllats.Add(new TropusiAlllat(5, "Jaguár", "Elíz", 64, "Nőstény"));
+        }
+        public void Szamolas()
+        {
+            int ossz = viziAllats.Count + hidegAllats.Count + mediterranAllats.Count + melegAllats.Count + tropusiAlllats.Count;
+            telitetseg = ossz;
+            for (int i = 0; i < capacity; i++)
+            {
+                if (i < telitetseg)
+                {
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.Write(" ");
+                }
+                else
+                {
+
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.Write(" ");
+                }
+                Console.ResetColor();
+            }
+        }
+    }
+}
--- Allatkert/Program.cs
+++ Allatkert/Program.cs
@@ -1,46 +1,12 @@
 ﻿namespace Allatkert
 {
-	internal class Program
-	{
-		static void Main(string[] args)
-		{
-            //Console.WriteLine("aaaaaaaaaaaaaaa");
-            //Console.Clear();
-            //Console.WriteLine("aaaa");
-
-
-            Console.WriteLine("O\nO");
-			int haladas = 0;
-			int haladas2 = 0;
-			for (int i = 0; i < 5; i++)
-            {
-				Thread.Sleep(1000);
-				Console.Clear();
-                Console.SetCursorPosition(haladas += 2, 0);
-                Console.WriteLine("O");
-				Console.SetCursorPosition(haladas2 += 3, 1);
-                Console.WriteLine("O");
-            }
-		}
-		static void Szazalek()
-		{
-			string[] szavak = new string[5];
-			szavak[0] = "aaaa";
-			szavak[1] = "bbb";
-			szavak[2] = "ccc";
-
-			Console.WriteLine("_______");
-			Console.Write("|");
-
-			for (int i = 0; i < szavak.Length; i++)
-			{
-				if (szavak[i] != null) Console.BackgroundColor = ConsoleColor.Green;
-				else Console.BackgroundColor = ConsoleColor.Black;
-				Console.Write(" ");
-			}
-			Console.BackgroundColor = ConsoleColor.Black;
-			Console.WriteLine("|");
-			Console.WriteLine("_______");
-		}
-	}
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Allatkert allatkert = new Allatkert();
+            allatkert.Beolvasas();
+            allatkert.Szamolas();
+        }
+    }
 }
